ab217b87a7c39f95f061d3019d7ef30fcbf404e7
Nikolai Trukhin <n.trukhin@uzum.com>
2022-12-22 14:13:42 +0100

    Initial Commit

---
+++ tddTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tddTask
+//
+//  Created by Nikolay Truhin on 22/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
